be513b772ed6cb300a90d8ca29dcf3dd9f3fb0e0
yang2019901 <1308592371@qq.com>
2024-12-27 16:05:07 +0800

    modify network structure, use cos_similarity instead of just get inner product

--- FeatMatchNN.py
+++ FeatMatchNN.py
@@ -17,15 +17,35 @@ def viz_frame(frame):
 
 class FeatMatchDataset(torch.utils.data.Dataset):
     def __init__(self, frames, H=None, W=None, dev=None):
-        self.x1, self.x2, self.pts1, self.pts2, self.valid2, self.cld2 = zip(*frames)
-        self.x1 = torch.stack(self.x1).to(dev)
-        self.x2 = torch.stack(self.x2).to(dev)
-        self.pts1 = torch.stack(self.pts1).to(dev)
-        self.pts2 = torch.stack(self.pts2).to(dev)
-        self.valid2 = torch.stack(self.valid2).to(dev)
-        self.cld2 = torch.stack(self.cld2).to(dev)
-        self.H = frames[0][0].shape[-2] if H is None else H
-        self.W = frames[0][0].shape[-1] if W is None else W
+        H0, W0 = frames[0][0].shape[-2:]
+        self.H = H0 if H is None else H
+        self.W = W0 if W is None else W
+        x1, x2, pts1, pts2, valid2, cld2 = zip(*frames)
+        x1 = torch.stack(x1).to(dev)
+        x2 = torch.stack(x2).to(dev)
+        pts1 = torch.stack(pts1).to(dev)
+        pts2 = torch.stack(pts2).to(dev)
+        valid2 = torch.stack(valid2).to(dev)
+        cld2 = torch.stack(cld2).to(dev)
+        x1, x2, cld2 = v2.functional.resize(
+            torch.stack([x1, x2, cld2.permute(0, 3, 1, 2)]), (self.H, self.W)
+        )
+        cld2 = cld2.permute(0, 2, 3, 1)
+        if self.H != H0:
+            pts1[..., 0] = pts1[..., 0] * self.H // H0
+            pts2[..., 0] = pts2[..., 0] * self.H // H0
+        if self.W != W0:
+            pts1[..., 1] = pts1[..., 1] * self.W // W0
+            pts2[..., 1] = pts2[..., 1] * self.W // W0
+        self.x1, self.x2, self.pts1, self.pts2, self.valid2, self.cld2 = (
+            x1,
+            x2,
+            pts1,
+            pts2,
+            valid2,
+            cld2,
+        )
+
         self.transforms = v2.Compose(
             [
                 v2.ColorJitter(brightness=0.5, contrast=0.5, saturation=0.5, hue=0.5),
@@ -46,19 +66,8 @@ class FeatMatchDataset(torch.utils.data.Dataset):
             self.valid2[idx],
             self.cld2[idx],
         )
-        H0, W0 = x1.shape[-2:]
-        x1, x2, cld2 = v2.functional.resize(
-            torch.stack([x1, x2, cld2.permute(2, 0, 1)]), (self.H, self.W)
-        )
-        cld2 = cld2.permute(1, 2, 0)
-        # # Note: the same transform for x1 and x2
+        # Note: the same transform for x1 and x2
         x1, x2 = self.transforms(torch.stack([x1, x2]))
-        if self.H != H0:
-            pts1[..., 0] = pts1[..., 0] * self.H // H0
-            pts2[..., 0] = pts2[..., 0] * self.H // H0
-        if self.W != W0:
-            pts1[..., 1] = pts1[..., 1] * self.W // W0
-            pts2[..., 1] = pts2[..., 1] * self.W // W0
         return x1, x2, pts1[:80], pts2[:80], valid2[:80], cld2
 
 
@@ -70,33 +79,26 @@ class FeatMatchNN(nn.Module):
 
     def __init__(self):
         super(FeatMatchNN, self).__init__()
-        c1, c2, c3, c4, c5 = 3, 8, 16, 32, 64
+        c1, c2, c3, c4, c5 = 3, 4, 8, 16, 16
         # Note whether receptive field is large enough. See https://blog.csdn.net/Rolandxxx/article/details/127270974
         self.conv_enc = nn.ModuleList(
             [
-                nn.Conv2d(c1, c2, kernel_size=3, padding=1),
+                nn.Conv2d(c1, c2, kernel_size=5, padding=2),
                 nn.Conv2d(c2, c3, kernel_size=5, padding=2),
-                nn.Conv2d(c3, c4, kernel_size=7, padding=3),
-                nn.Conv2d(c4, c5, kernel_size=9, padding=4),
+                nn.Conv2d(c3, c4, kernel_size=5, padding=2),
+                nn.Conv2d(c4, c5, kernel_size=5, padding=2),
             ]
         )
         self.conv_dec = nn.ModuleList(
             [
-                nn.ConvTranspose2d(
-                    c5, c4, kernel_size=3, stride=2, padding=1, output_padding=1
-                ),
-                nn.ConvTranspose2d(
-                    c4, c3, kernel_size=3, stride=2, padding=1, output_padding=1
-                ),
-                nn.ConvTranspose2d(
-                    c3, c2, kernel_size=3, stride=2, padding=1, output_padding=1
-                ),
-                nn.ConvTranspose2d(
-                    c2, 1, kernel_size=3, stride=2, padding=1, output_padding=1
-                ),
+                nn.Conv2d(2 * c5 + 1, c4, kernel_size=3, padding=1),
+                nn.Conv2d(2 * c4 + 1, c3, kernel_size=3, padding=1),
+                nn.Conv2d(2 * c3 + 1, c2, kernel_size=3, padding=1),
+                nn.Conv2d(2 * c2 + 1, 1, kernel_size=3, padding=1),
             ]
         )
         self.attns = {}
+        self.feats = {}
 
     def forward(self, x1, x2, pts1):
         """
@@ -109,49 +111,80 @@ class FeatMatchNN(nn.Module):
         B, L = pts1.shape[:2]
         H, W = x1.shape[-2:]
         # copy and reshape
-        h1 = x1.clone().detach().view(B, 3, H, W)
-        h2 = x2.clone().detach().view(B, 3, H, W)
-        pts = pts1.clone().detach().to(torch.int64).view(B, L, 2)
+        h1 = x1.view(B, 3, H, W)
+        h2 = x2.view(B, 3, H, W)
+        pts = pts1.clone().to(torch.int64).view(B, L, 2)
         # encode h1, h2, and calc attn with h1[pts1] and h2 of each layer
         for i, layer in enumerate(self.conv_enc):
             Ci, Hi, Wi = h1.shape[-3:]
-            # Conv-ReLU-Pool once
-            h1 = F.relu(layer(h1))
-            h2 = F.relu(layer(h2))
+            # Conv-ReLU
+            h1 = F.leaky_relu(layer(h1))
+            h2 = F.leaky_relu(layer(h2))
+            # save feats
+            self.feats[i] = h2
+            # downsample
             h1 = F.max_pool2d(h1, 2)
             h2 = F.max_pool2d(h2, 2)
             # scale indices
             Ci_, Hi_, Wi_ = h1.shape[-3:]
             pts[..., 0] = pts[..., 0] * Hi_ // Hi
             pts[..., 1] = pts[..., 1] * Wi_ // Wi
-            # get Q that is used to calculate attention, Q: (B, Ci_, L)
-            # Q[i, j, k] = h1[i, j, pts[i, k, 0], pts[i, k, 1]]
-            Q = h1.view(B, Ci_, Hi_ * Wi_).gather(
-                2, (pts[:, :, 0] * Wi_ + pts[:, :, 1]).view(B, 1, L)
+            # get query that is used to calculate attention, query: (B, Ci_, L)
+            query = h1.view(B, Ci_, Hi_ * Wi_).gather(
+                2, (pts[:, :, 0] * Wi_ + pts[:, :, 1]).unsqueeze(1).expand(-1, Ci_, -1)
             )
-            # attn = F.sigmoid(torch.einsum("bchw,bcl->blhw", h2, Q))
-            # attns.append(attn.reshape(-1, 1, Hi_, Wi_))
-            attn = F.softmax(
-                torch.einsum("bchw,bcl->blhw", h2, Q).view(B, L, -1), dim=-1
+
+            attn = F.cosine_similarity(
+                h2.view(B, Ci_, Hi_ * Wi_, 1).permute(0, 2, 3, 1),
+                query.permute(0, 2, 1).unsqueeze(1),
+                dim=-1,
             )
-            self.attns[i] = attn.view(B * L, 1, Hi_, Wi_)
+            attn = attn.view(B, Hi_, Wi_, L).permute(0, 3, 1, 2)  # (B, L, H, W)
+
+            # attn = torch.einsum("bchw,bcl->blhw", h2, query)
+
+            # self.viz_featmap(attn[0, 0], "attn")
+            # self.viz_featmap(h2[0, 3], "h2")
+            # self.viz_featmap(h1[0, 3], "h1", pts[0, 0])
+            # plt.show()
+            self.attns[i] = attn.reshape(B * L, 1, Hi_, Wi_)
         # decode h2 with attns
         h2 = h2.repeat(L, 1, 1, 1)  # (B, Ci, Hi, Wi) -> (B*L, Ci, Hi, Wi)
         for i, layer in enumerate(self.conv_dec):
-            attn = self.attns[len(self.conv_enc) - i - 1]
-            # h2: (B*L, Ci, Hi, Wi), attn: (B*L, 1, Hi, Wi)
-            h2 = layer(h2 * attn)
-            if i < len(self.conv_dec) - 1:
-                h2 = F.relu(h2)
-        # h2: (B*L, 1, H, W), logits: (B, L, H, W)
+            idx = len(self.conv_enc) - i - 1
+            # concat attn and h2. h2: (B*L, Ci, Hi, Wi), attn: (B*L, 1, Hi, Wi)
+            attn = self.attns[idx]
+            h2 = torch.concat([h2, attn], dim=1)
+            # upsampling
+            h2 = F.interpolate(h2, scale_factor=2)
+            # concat with self.feats[i]
+            feat = self.feats[idx].repeat(L, 1, 1, 1)
+            h2 = torch.concat([h2, feat], dim=1)
+            # Conv-ReLU
+            h2 = F.leaky_relu(layer(h2)) if i < len(self.conv_dec) - 1 else layer(h2)
+        # h2: (B*L, 1, H, W) -> (B, L, H, W)
         logits = h2.view(B, L, H, W)
         return logits
 
+    def viz_featmap(self, featmap: torch.Tensor, title="", pts=None):
+        # featmap: (H, W)
+        fig, ax = plt.subplots()
+        cbar = fig.colorbar(
+            ax.imshow(featmap.cpu().detach()), ax=ax, orientation="vertical"
+        )
+        if pts is not None:
+            p = pts.cpu().detach()
+            ax.scatter(p[..., 1], p[..., 0], c="r", s=4)
+        fig.suptitle(title)
+
     def visualize(self, x1, x2, pts1, pts2, valid2):
         logits = self.forward(x1, x2, pts1)
+        B, L, H, W = logits.shape
         img1, img2 = x1[0].permute(1, 2, 0).cpu(), x2[0].permute(1, 2, 0).cpu()
         pt1, pt2, pt2_vis = pts1[0, 0].cpu(), pts2[0, 0].cpu(), int(valid2[0, 0])
-        heatmap = F.sigmoid(logits[0, 0, :, :]).cpu().detach()
+        heatmap = (
+            F.softmax(logits[0, 0, :, :].view(H * W), dim=-1).view(H, W).cpu().detach()
+        )
         fig, axes = plt.subplots(
             1, 2, figsize=(4.5 * 2 * x1.shape[-1] / x1.shape[-2], 4.5)
         )
@@ -164,18 +197,45 @@ class FeatMatchNN(nn.Module):
         axes[1].scatter(pt2[1], pt2[0], c="r", s=4, label="pt2", alpha=pt2_vis)
         plt.show()
 
-    def fuck_loss(self, x1, x2, pts1, pts2, valid2):
+    def loss_2d(self, x1, x2, pts1, pts2, valid2, cld2):
+        assert torch.all(valid2), "valid2 should be all True for now"
         logits = self.forward(x1, x2, pts1)
         B, L, H, W = logits.shape
-        prob_map = F.sigmoid(logits)  # (B, L, H, W)
-        probs = (
-            prob_map.view(B, L, -1)
-            .gather(-1, (pts2[..., 0] * W + pts2[..., 1]).view(B, L, 1))
-            .view(B, L)
-        )  # (B, L)
-        l1 = (~valid2) * torch.sum(prob_map.view(B, L, -1), -1)
-        l2 = valid2 * (1 - probs)
-        return torch.mean(l1 + l2)
+        grid = torch.meshgrid(
+            torch.arange(H, dtype=torch.float, device=x1.device),
+            torch.arange(W, dtype=torch.float, device=x1.device),
+            indexing="ij",
+        )
+        grid = torch.stack(grid, dim=-1)
+        dist = torch.norm(
+            grid.view(1, 1, H * W, 2) - pts2.view(B, L, 1, 2), dim=-1, p=1
+        )
+        dist = dist.view(B * L, -1)
+        logits = logits.view(B * L, -1)
+        loss2d = torch.sum(F.softmax(logits, dim=-1) * dist) / (B * L)
+        return loss2d
+
+    def loss_3d(self, x1, x2, pts1, pts2, valid2, cld2):
+        assert torch.all(valid2), "valid2 should be all True for now"
+        logits = self.forward(x1, x2, pts1)
+        B, L, H, W = logits.shape
+        cld = cld2.view(B, H * W, 3)
+        valid = valid2.view(B, L, 1)
+        pts = pts2.view(B, L, 2)
+        # pts2_3d: (B, L, 3)
+        pts2_3d = valid * cld.gather(
+            1, (pts[..., 0] * W + pts[..., 1]).unsqueeze(-1).expand(-1, -1, 3)
+        )
+        # dist: (B, L, H*W)
+        dist = torch.norm(pts2_3d.view(B, L, 1, 3) - cld.view(B, 1, H * W, 3), dim=-1)
+
+        log_softmax = F.log_softmax(logits.view(B * L, -1), dim=-1)
+        log_probs = log_softmax.gather(
+            1, (pts[..., 0] * W + pts[..., 1]).view(B * L, 1)
+        )
+        loss3d = -torch.mean(log_probs)
+
+        return loss3d
 
 
 def main():
@@ -187,35 +247,35 @@ def main():
         frames += torch.load(f"{dataset_dir}/{f}", weights_only=True)
         print(f"{f} loaded.")
     # create dataset
-    dataset = FeatMatchDataset(frames, H=480, W=640, dev=dev)
+    dataset = FeatMatchDataset(frames, H=128, W=128, dev=dev)
     # create dataloader
     dataloader = DataLoader(dataset, batch_size=2, shuffle=True)
     # create model
     model = FeatMatchNN().to(dev)
     try:
-        model.load_state_dict(torch.load("fuck_featmatch.pt", weights_only=True))
+        model.load_state_dict(torch.load("fuck.pt", weights_only=True))
     except:
         print("weights not found. fresh train.")
     # train
-    optimizer = torch.optim.Adam(model.parameters(), lr=1e-1)
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
     for epoch in range(1000):
         optimizer.zero_grad()
         for x1, x2, pts1, pts2, valid2, cld2 in dataloader:
-            loss = model.fuck_loss(x1, x2, pts1, pts2, valid2)
+            loss = model.loss_2d(x1, x2, pts1, pts2, valid2, cld2)
             loss.backward()
         optimizer.step()
-        print(f"epoch {epoch} loss: {loss.item():.3f}")
-        if epoch % 100 == 0:
+        print(f"\tepoch {epoch} loss: {loss.item():.3f}")
+        if (epoch + 1) % 50 == 0:
             model.visualize(x1, x2, pts1, pts2, valid2)
-            torch.save(model.state_dict(), "fuck_featmatch.pt")
+            torch.save(model.state_dict(), "fuck.pt")
 
 
 def test():
     # Note: ensure that H and W is power of 2
     torch.manual_seed(0)
-    H, W = 256, 256
-    L = 10
-    B = 1
+    H, W = 480, 640
+    L = 100
+    B = 2
     model = FeatMatchNN()
     x1 = torch.rand(B, 3, H, W)
     x2 = torch.rand(B, 3, H, W)
